2460be24d3d6747b95db8e1159b59cb3de98625f
REIGaruki <gyakujo@mail.ru>
2024-07-02 21:46:25 +0300

    Services

--- src/main/java/com/example/exam/service/ExaminerServiceImpl.java
+++ src/main/java/com/example/exam/service/ExaminerServiceImpl.java
@@ -4,11 +4,25 @@ import com.example.exam.domain.Question;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService{
+    JavaQuestionService javaQuestionService;
     @Override
     public Collection<Question> getQuestions(int amount) {
-        return null;
+        if (amount > javaQuestionService.getCollectionSize()) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            Random random = new Random();
+            Set<Question> randomQuestions = new HashSet<>();
+            while (randomQuestions.size() < amount) {
+                int randomId = random.nextInt(javaQuestionService.getCollectionSize());
+                randomQuestions.add(javaQuestionService.getRandomQuestion());
+            }
+            return randomQuestions;
+        }
     }
 }
--- src/main/java/com/example/exam/service/JavaQuestionService.java
+++ src/main/java/com/example/exam/service/JavaQuestionService.java
@@ -3,32 +3,63 @@ package com.example.exam.service;
 import com.example.exam.domain.Question;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.*;
 
 @Service
 public class JavaQuestionService implements QuestionService{
+    public static List<Question> javaQuestions = new ArrayList<>(Arrays.asList(
+            new Question("Что такое «переменная»?",
+                    "Это переменная"),
+            new Question("По каким параметрам переменные различаются?",
+                    "Параметры"),
+            new Question("Перечислите типы переменных и действия, которые с ними можно осуществлять.",
+                    "Типы и действия"),
+            new Question("Что означает \"инициализация\"?",
+                    "Означает инициалтзацию"),
+            new Question("Какие особенности инициализации вы можете назвать?",
+                    "Никакие"),
+            new Question("Какие условные операторы вы знаете? Дайте краткое определение каждому из них.",
+                    "Краткие определения"),
+            new Question("Что такое \"цикл\"?",
+                    "Это цикл"),
+            new Question("Ка. они разныекие циклы вы знаете, в чем их отличия?",
+                    "Знаю все"),
+            new Question("Что вы знаете о массивах?",
+                    "ЧТо они масивные"),
+            new Question("Что такое \"метод\"?",
+                    "Сериал")
+    ));
     @Override
     public Question add(String question, String answer) {
-        return null;
+        Question newQuestion = new Question(question, answer);
+        javaQuestions.add(newQuestion);
+        return newQuestion;
     }
 
     @Override
     public Question add(Question question) {
-        return null;
+        javaQuestions.add(question);
+        return question;
     }
 
     @Override
     public Question remove(Question question) {
-        return null;
+        javaQuestions.remove(question);
+        return question;
     }
 
     @Override
-    public Collection<Question> getAll() {
-        return null;
+    public List<Question> getAll() {
+        return javaQuestions;
+    }
+    public int getCollectionSize() {
+        return javaQuestions.size();
     }
 
     @Override
     public Question getRandomQuestion() {
-        return null;
+        Random random = new Random();
+        int randomId = random.nextInt(getCollectionSize());
+        return javaQuestions.get(randomId);
     }
 }
