1d0bcb0050232eb3d458c027f9c078aa4eaf000d
REIGaruki <gyakujo@mail.ru>
2024-07-02 19:32:55 +0300

    Controllers

--- src/main/java/com/example/exam/controller/ExamController.java
+++ src/main/java/com/example/exam/controller/ExamController.java
@@ -1,7 +1,17 @@
 package com.example.exam.controller;
 
+import com.example.exam.domain.Question;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class ExamController {
+    ExamController examController;
+    @GetMapping("/get/{amount}")
+    Collection<Question> getQuestions(@PathVariable("amount") int amount) {
+        return examController.getQuestions(amount);
+    }
 }
--- src/main/java/com/example/exam/controller/JavaController.java
+++ src/main/java/com/example/exam/controller/JavaController.java
@@ -1,7 +1,33 @@
 package com.example.exam.controller;
 
+import com.example.exam.domain.Question;
+import com.example.exam.service.JavaQuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
+@RequestMapping(path="/java")
 public class JavaController {
+    JavaQuestionService javaQuestionService;
+    @GetMapping("/add")
+    public Question add(String question, String answer) {
+        return javaQuestionService.add(question, answer);
+    }
+
+    @GetMapping("/remove")
+    public Question remove(Question question) {
+        return javaQuestionService.remove(question);
+    }
+
+    public Collection<Question> getAll() {
+        return javaQuestionService.getAll();
+    }
+
+    @GetMapping("/random")
+    public Question getRandomQuestion() {
+        return javaQuestionService.getRandomQuestion();
+    }
 }
